d33a24417e8b02048c1d0b8f5b472b06fb315446
iskybow <kjionmd@gmail.com>
2018-07-30 15:24:02 +0300

    Merge branch 'fixes' of https://github.com/kennerus/artcraft into fixes

--- js/sticky-script.js
+++ js/sticky-script.js
@@ -1,20 +1,20 @@
-// var stickySidebar = new StickySidebar('#sidebar', {
-//   screenlimit: true,
-//   topSpacing: 20,
-//   bottomSpacing: 20,
-//   containerSelector: '.main',
-//   innerWrapperSelector: '.sidebar__inner'
-// });
+var stickySidebar = new StickySidebar('#sidebar', {
+  topSpacing: 20,
+  bottomSpacing: 20,
+  containerSelector: '.main',
+  innerWrapperSelector: '.sidebar__inner',
+  screenlimit: true
+});
 
 
-$(window).resize(function() {
-  if ( $(window).width() > 769 ) {
-
-    var stickySidebar = new StickySidebar('#sidebar', {
-      screenlimit: true,
-      topSpacing: 20,
-      bottomSpacing: 20,
-      containerSelector: '.main',
-      innerWrapperSelector: '.sidebar__inner'
-    });
-  }});
+// $(window).resize(function() {
+//   if ( $(window).width() > 769 ) {
+//
+//     var stickySidebar = new StickySidebar('#sidebar', {
+//       screenlimit: true,
+//       topSpacing: 20,
+//       bottomSpacing: 20,
+//       containerSelector: '.main',
+//       innerWrapperSelector: '.sidebar__inner'
+//     });
+//   }});
